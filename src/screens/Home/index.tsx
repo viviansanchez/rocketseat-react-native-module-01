2d419ade11b814c68319d463144666ec3acd7edf
Vivian Sanchez <vivian.sanchez.dev@gmail.com>
2025-04-01 14:14:51 -0300

    feat: handleParticipantAdd now actually adds a participant

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -6,28 +6,24 @@ import {
   FlatList,
   Alert,
 } from "react-native";
+import { useState } from "react";
 import { styles } from "./styles";
 import { Participant } from "../../components/Participant";
 
 export function Home() {
-  const participants = [
-    "Vivian",
-    "Rodrigo",
-    "Jorge",
-    "Diego",
-    "Ana",
-    "Isa",
-    "Alexandre",
-    "Luciana",
-  ];
+  const [participants, setParticipants] = useState<string[]>([]);
+  const [participantName, setParticipantName] = useState("");
 
   function handleParticipantAdd() {
-    if (participants.includes("Rodrigo")) {
+    if (participants.includes(participantName)) {
       return Alert.alert(
         "Participante existe",
         "Já existe um participante na lista com esse nome"
       );
     }
+
+    setParticipants((prevState) => [...prevState, participantName]);
+    setParticipantName("");
   }
 
   function handleParticipantRemove(name: string) {
@@ -52,6 +48,8 @@ export function Home() {
           style={styles.input}
           placeholder="Nome do participante"
           placeholderTextColor={"#6B6B6B"}
+          onChangeText={setParticipantName}
+          value={participantName}
         />
         <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
           <Text style={styles.buttonText}>+</Text>
